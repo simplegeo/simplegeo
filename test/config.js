ed08d2bfbffbf2f2e8aed6109045b8df41f5a29f
Isaac Hildebrandt <isaac@wimgo.com>
2011-04-19 17:18:57 -0500

    expand functionality, add record interface, add convenience objects for layers,records, and features

--- test/config.js
+++ test/config.js
@@ -1,2 +1,2 @@
-exports.key=false;
-exports.secret=false;
+exports.key='rRHhsfsDWUt3MhfQ2MAQGGreMUY6LpvE';
+exports.secret='MaTKuCb3k6h25aVqGkhHhQ74HXPY64ne';
--- test/simplegeo.test.js
+++ test/simplegeo.test.js
@@ -15,13 +15,42 @@ if (!config.key || !config.secret) {
     return;
 }
 
-function create() {
+function createAuthClient() {
     return new simplegeo.SimpleGeo(config.key,config.secret);
+}
+
+function createUnauthClient() {
+    return new simplegeo.SimpleGeo();
 }
 
 module.exports = {
     createClient: function() {
-        var sg = create();
+        var sg = createAuthClient();
         assert.isNotNull(sg);
+
+        var sg = createUnauthClient();
+        assert.isNotNull(sg);
+    },
+
+    unauthClient: function () {
+        var sg = createUnauthClient();
+        assert.throws(function(){
+            sg.getLayers();    
+        });
+    },
+
+    testDataErrorCallback: function () {
+        var sg = createUnauthClient();
+        assert.throws(function(){
+            sg.getFeatureDetails();
+        });
+        sg.getFeatureDetails('fail',function (e,d,r) {
+            assert.isNotNull(e);
+            assert.isNull(d);
+        });
+        sg.getFeatureDetails('SG_2AziTafTLNReeHpRRkfipn',function (e,d,r) {
+            assert.isNotNull(d);
+            assert.isNull(e);
+        });
     }
 }
